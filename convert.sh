a9f729db822b498b1427f10f71a924bbbea16e1c
nic <nwanavit@hatari.cc>
2020-01-28 15:29:52 +0700

    experimenting with md and html pandoc converter

---
+++ convert.sh
@@ -0,0 +1,1 @@
+pandoc markdown_file.md --pdf-engine=xelatex -t latex --include-in-header=fontoptions.tex -f markdown_mmd -o test_pdf.pdf
---
+++ convertHtml.sh
@@ -0,0 +1,1 @@
+pandoc ./template_html_typora.html -t html --include-in-header=fontoptions.tex -o test_pdf.pdf
