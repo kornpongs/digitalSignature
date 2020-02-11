df225a41bffedeb2ccc26288c4e38232729a38b2
nic <nwanavit@hatari.cc>
2020-02-11 13:29:41 +0700

    cleanup folder and add open command for convert.sh

--- convert.sh
+++ convert.sh
@@ -1,1 +1,3 @@
-pandoc markdown_file.md --pdf-engine=xelatex -t latex --include-in-header=fontoptions.tex -f markdown_mmd -o test_pdf.pdf
+#pandoc markdown_file.md --pdf-engine=xelatex -t latex --include-in-header=fontoptions.tex -f markdown_mmd -css github.css -o test_pdf.pdf
+markdown-pdf -s github.css -o test_pdf.pdf -f A3 markdown_file.md
+open test_pdf.pdf
--- convertHtml.sh
+++
@@ -1,1 +0,0 @@
-pandoc ./template_html_typora.html -t html --include-in-header=fontoptions.tex -o test_pdf.pdf
